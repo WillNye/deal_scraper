1390a163daa12bf61efad4970cf91152a46e011f
will <wbeasley@stealthmonitoring.com>
2019-02-14 21:07:37 -0600

    Added filter mappings, updated arg name in Config.setup

--- config.py
+++ config.py
@@ -6,6 +6,36 @@ from elasticsearch import Elasticsearch
 ES = Elasticsearch()
 EP = 'https://brickseek.com/{}'
 
+FILTER_MAPPINGS = {
+    'All': {'url': '', 'index_name': 'all'},
+    'Apparel': {'url': '&categories[1]=1', 'index_name': 'apparel'},
+    'Appliances': {'url': '&categories[2]=2', 'index_name': 'appliances'},
+    'Automotive': {'url': '&categories[3]=3', 'index_name': 'automotive'},
+    'Baby': {'url': '&categories[4]=4', 'index_name': 'baby'},
+    'Electronics': {'url': '&categories[7]=7', 'index_name': 'electronics'},
+    'Outdoors': {'url': '&categories[15]=15', 'index_name': 'outdoors'},
+    'Grocery': {'url': '&categories[12]=12', 'index_name': 'grocery'},
+    'Health': {'url': '&categories[5]=5', 'index_name': 'health'},
+    'Home': {'url': '&categories[9]=9', 'index_name': 'home'},
+    'Renovation': {'url': '&categories[10]=10', 'index_name': 'renovation'},
+    'Household': {'url': '&categories[8]=8', 'index_name': 'household'},
+    'Pet': {'url': '&categories[16]=16', 'index_name': 'pet'},
+    'Sporting': {'url': '&categories[17]=17', 'index_name': 'sporting'},
+    'Tools': {'url': '&categories[18]=18', 'index_name': 'tools'},
+    'Toys': {'url': '&categories[19]=19', 'index_name': 'toys'},
+    'Apple': {'url': '&brands[7]=7', 'index_name': 'apple'},
+    'Barbie': {'url': '&brands[265]=265', 'index_name': 'barbie'},
+    'Disney': {'url': '&brands[13]=13', 'index_name': 'disney'},
+    'GE': {'url': '&brands[54]=54', 'index_name': 'ge'},
+    'Kobalt': {'url': '&brands[6572]=6572', 'index_name': 'kobalt'},
+    'Lego': {'url': '&brands[56]=56', 'index_name': 'lego'},
+    'Nintendo': {'url': '&brands[11]=11', 'index_name': 'nintendo'},
+    'Pillowfort': {'url': '&brands[3831]=3831', 'index_name': 'pillowfort'},
+    'OzarkTrail': {'url': '&brands[50]=50', 'index_name': 'ozark_trail'},
+    'Samsung': {'url': '&brands[42]=42', 'index_name': 'samsung'},
+    'PioneerWoman': {'url': '&brands[137]=137', 'index_name': 'pioneer_woman'}
+}
+
 
 class Config:
     ES_INDEX = None
@@ -13,12 +43,8 @@ class Config:
     ZIP_CODE = None
 
     @staticmethod
-    def setup(brand=None, category=None):
-        es_index = 'product'
-        if category:
-            es_index = es_index + '_{}'.format(category)
-        if brand:
-            es_index = es_index + '_{}'.format(brand)
+    def setup(filter_index=None):
+        es_index = 'product_{}'.format(filter_index)
 
         Config.ES_INDEX = es_index
         ES.indices.create(Config.ES_INDEX, ignore=400)
